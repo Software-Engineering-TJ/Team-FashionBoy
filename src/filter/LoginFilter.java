80ea3446a3bab2c570ed54e2b3b50484bfe3e2b7
1654364091 <1654364091@qq.com>
2021-11-16 15:49:33 +0800

    添加了一个登录检测过滤器——用于访问其他页面时检测是否登录了

--- src/filter/LoginFilter.java
+++ src/filter/LoginFilter.java
@@ -1,6 +1,7 @@
 package filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.io.IOException;
 /**
  * 用于过滤没有登录的用户
  */
+@WebFilter(filterName = "LoginFilter",urlPatterns = {"/pages/*"})
 public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
