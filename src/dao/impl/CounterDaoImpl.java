5a6c525b2cec7e437b1b020e87c860aef7c7187f
HJK <1654364091@qq.com>
2022-01-09 14:18:45 +0800

    修复开课bug(求求了，别再覆盖了)

--- src/dao/impl/CounterDaoImpl.java
+++ src/dao/impl/CounterDaoImpl.java
@@ -11,19 +11,19 @@ public class CounterDaoImpl extends BaseDao implements CounterDao {
 
     @Override
     public int UpdateAllOfCounter(int courseID, int classID) {
-        String sql = "update Counter set courseID = ? , classID = ? where id = 1";
+        String sql = "update counter set courseID = ? , classID = ? where id = 1";
         return update(sql,courseID,classID);
     }
 
     @Override
     public int UpdateCourseIDOfCounter(int courseID) {
-        String sql = "update Counter set courseID = ? where id = 1";
+        String sql = "update counter set courseID = ? where id = 1";
         return update(sql,courseID);
     }
 
     @Override
     public int UpdateClassIDOfCounter(int classID) {
-        String sql = "update Counter set classID = ? where id = 1";
+        String sql = "update counter set classID = ? where id = 1";
         return update(sql,classID);
     }
 }
